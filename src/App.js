fd78ab8b7b86cc61905e3bec363b6e4d6d664315
Grimff <gm14x.miller@gmail.com>
2024-12-31 03:41:10 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,50 +1,166 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import champs from "./data/champs";
 
 const App = () => {
-  const [metar, setMetar] = useState(null);
-  const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchMETAR = async () => {
-      const BASE_URL = "https://corsproxy.io/?url=https://aviationweather.gov/adds/dataserver_current/httpparam";
-
-      try {
-        const response = await axios.get(BASE_URL, {
-          params: {
-            dataSource: "metars",
-            requestType: "retrieve",
-            format: "xml",
-            stationString: "KBNA",
-            hoursBeforeNow: 1,
-            mostRecentForEachStation: "true",
-          },
+    const [trait1, setTrait1] = useState(null);
+    const [trait2, setTrait2] = useState(null);
+    const [kingUnit, setKingUnit] = useState(null);
+  
+    const ReturnTraitsOfChamp = (champ) => {
+        let selected = [];
+        Object.values(champs).forEach(element => {
+            let found = element.find(item => item.name === champ);
+            if (found !== undefined) {
+                selected.push(found);
+            }
         });
-        setMetar(response.data); // Store the METAR data
-      } catch (err) {
-        setError("Failed to fetch METAR data. Please check the API or network.");
-      }
-    };
+        return selected[0].traits;
+    }
+  
+    const ReturnCostOfChamp = (champ) => {
+        let selected = [];
+        Object.values(champs).forEach(element => {
+            let found = element.find(item => item.name === champ);
+            if (found !== undefined) {
+                selected.push(found);
+            }
+        });
+        return selected[0].cost;
+    }
 
-    fetchMETAR();
-  }, []);
+    const ReturnChampsWithTrait = (trait) => {
+        let selected = [];
+        Object.values(champs).forEach(element => {
+            let found = element.filter(item => item.traits.indexOf(trait) > -1);
+            found.forEach(champ => {
+                if (found !== champ) {
+                    selected.push(champ);
+                }
+            })
+        });
+        return selected;
+    }
+    
+    const ReturnAllTraits = () => {
+        let selected = [];
+        Object.values(champs).forEach(element => {
+            Object.values(element).forEach(champ => {
+                champ.traits.forEach(trait => {
+                    if (selected.indexOf(trait) > -1) {
+                        // do nothing if in array 
+                    } else {
+                        selected.push(trait);
+                    }
+                })
+            });
+        });
+        return selected;
+    }
 
-  return (
-    <div>
-      <h1>METAR Test for KBNA...</h1>
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      {!error && (
+    function randomIntFromInterval(min, max) { // min and max included 
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    const SelectRandomTrait = () => {
+        let allTraits = ReturnAllTraits();
+        let viableTraits = []
+        allTraits.forEach(trait => {
+            if (ReturnChampsWithTrait(trait).length !== 1) {
+                viableTraits.push(trait);
+            }
+        });
+        let randomNum = randomIntFromInterval(0, viableTraits.length - 1);
+        return viableTraits[randomNum];
+    }
+
+    const SelectRandomSecondaryTrait = (excludedTrait) => {
+        let allTraits = ReturnAllTraits();
+        let viableTraits = []
+        allTraits.forEach(trait => {
+            if (ReturnChampsWithTrait(trait).length !== 1 || trait !== excludedTrait) {
+                viableTraits.push(trait);
+            }
+        });
+        let randomNum = randomIntFromInterval(0, viableTraits.length - 1);
+        return viableTraits[randomNum];
+    }
+
+    const RollComps = () => {
+        let randomTrait = SelectRandomTrait();
+        setTrait1(randomTrait);
+        let randomTrait2 = SelectRandomSecondaryTrait(trait1);
+        setTrait2(randomTrait2);
+        PickKingUnit(randomTrait, randomTrait2);
+    }
+
+    const PickKingUnit = (selectedTrait1, selectedTrait2) => {
+        let allUnits = [];
+        ReturnChampsWithTrait(selectedTrait1).forEach(champ => {
+            if (allUnits.indexOf(champ) === -1) {
+                allUnits.push(champ);
+            }
+        })
+        ReturnChampsWithTrait(selectedTrait2).forEach(champ => {
+            if (allUnits.indexOf(champ) === -1) {
+                allUnits.push(champ);
+            }
+        })
+        // USE THIS SECTION IF YOU WANT IT TO BE A 2 COST KING
+        // -------------------------------------------------------------------------
+
+        // let viableUnits = [];
+        // allUnits.forEach(champ => {
+        //     if (ReturnCostOfChamp(champ.name) === 2) {
+        //         viableUnits.push(champ);
+        //     }
+        // })
+        // setKingUnit(viableUnits[randomIntFromInterval(0, viableUnits.length - 1)]);
+
+        // -------------------------------------------------------------------------
+        // OTHERWISE USE THIS ONE
+        // -------------------------------------------------------------------------
+
+        setKingUnit(allUnits[randomIntFromInterval(0, allUnits.length - 1)]);
+        
+        // -------------------------------------------------------------------------
+    }
+
+    return (
         <div>
-          <h2>METAR Data:</h2>
-          {metar ? (
-            <pre>{metar}</pre>
-          ) : (
-            <p>Loading METAR data for KBNA</p>
-          )}
+            <button onClick={() => {
+                RollComps();
+            }}>
+                roll
+            </button>
+
+            {kingUnit !== null && kingUnit !== undefined ? 
+                <div>
+                    <strong>King Unit:</strong>
+                    {kingUnit.name}
+                </div>
+            : <></>}
+
+            {trait1 !== null ? 
+                <div>
+                    <strong>{trait1}</strong>
+                    {ReturnChampsWithTrait(trait1).map((champ, index) => {
+                        return (<div key={index}>{champ.name}</div>)
+                    })}
+                </div> 
+            : <></>}
+
+            {trait2 !== null ? 
+                <div>
+                    <strong>{trait2}</strong>
+                    {ReturnChampsWithTrait(trait2).map((champ, index) => {
+                        return (<div key={index}>{champ.name}</div>)
+                    })}
+                </div> 
+            : <></>}
+
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default App;
---
+++ src/data/champs.js
@@ -0,0 +1,330 @@
+var champs = {
+    "one": [
+        {
+            "name": "Amumu",
+            "traits": ["Automata", "Watcher"],
+            "cost": 1
+        },
+        {
+            "name": "Darius",
+            "traits": ["Conqueror", "Watcher"],
+            "cost": 1
+        },
+        {
+            "name": "Draven",
+            "traits": ["Conqueror", "Pit Fighter"],
+            "cost": 1
+        },
+        {
+            "name": "Irelia",
+            "traits": ["Rebel", "Sentinel"],
+            "cost": 1
+        },
+        {
+            "name": "Lux",
+            "traits": ["Academy", "Sorcerer"],
+            "cost": 1
+        },
+        {
+            "name": "Maddie",
+            "traits": ["Enforcer", "Sniper"],
+            "cost": 1
+        },
+        {
+            "name": "Morgana",
+            "traits": ["Black Rose", "Visionary"],
+            "cost": 1
+        },
+        {
+            "name": "Powder",
+            "traits": ["Family", "Scrap", "Ambusher"],
+            "cost": 1
+        },
+        {
+            "name": "Singed",
+            "traits": ["Chem-Baron", "Sentinel"],
+            "cost": 1
+        },
+        {
+            "name": "Steb",
+            "traits": ["Enforcer", "Bruiser"],
+            "cost": 1
+        },
+        {
+            "name": "Trundle",
+            "traits": ["Scrap", "Bruiser"],
+            "cost": 1
+        },
+        {
+            "name": "Vex",
+            "traits": ["Rebel", "Visionary"],
+            "cost": 1
+        },
+        {
+            "name": "Violet",
+            "traits": ["Family", "Pit Fighter"],
+            "cost": 1
+        },
+        {
+            "name": "Zyra",
+            "traits": ["Experiment", "Sorcerer"],
+            "cost": 1
+        }
+    ],
+    "two": [
+        {
+            "name": "Akali",
+            "traits": ["Rebel", "Quickstriker"],
+            "cost": 2
+        },
+        {
+            "name": "Camille",
+            "traits": ["Enforcer", "Ambusher"],
+            "cost": 2
+        },
+        {
+            "name": "Leona",
+            "traits": ["Academy", "Sentinel"],
+            "cost": 2
+        },
+        {
+            "name": "Nocturne",
+            "traits": ["Automata", "Quickstriker"],
+            "cost": 2
+        },
+        {
+            "name": "Rell",
+            "traits": ["Conqueror", "Sentinel", "Visionary"],
+            "cost": 2
+        },
+        {
+            "name": "Renata Glasc",
+            "traits": ["Chem-Baron", "Visionary"],
+            "cost": 2
+        },
+        {
+            "name": "Sett",
+            "traits": ["Rebel", "Bruiser"],
+            "cost": 2
+        },
+        {
+            "name": "Tristana",
+            "traits": ["Emissary", "Artillerist"],
+            "cost": 2
+        },
+        {
+            "name": "Urgot",
+            "traits": ["Experiment", "Pit Fighter", "Artillerist"],
+            "cost": 2
+        },
+        {
+            "name": "Vander",
+            "traits": ["Family", "Watcher"],
+            "cost": 2
+        },
+        {
+            "name": "Vladimir",
+            "traits": ["Black Rose", "Watcher", "Sorcerer"],
+            "cost": 2
+        },
+        {
+            "name": "Zeri",
+            "traits": ["Firelight", "Sniper"],
+            "cost": 2
+        },
+        {
+            "name": "Ziggs",
+            "traits": ["Scrap", "Dominator"],
+            "cost": 2
+        }
+    ],
+    "three": [
+        {
+            "name": "Blitzcrank",
+            "traits": ["Automata", "Dominator"],
+            "cost": 3
+        },
+        {
+            "name": "Cassiopeia",
+            "traits": ["Black Rose", "Dominator"],
+            "cost": 3
+        },
+        {
+            "name": "Ezreal",
+            "traits": ["Academy", "Rebel", "Artillerist"],
+            "cost": 3
+        },
+        {
+            "name": "Gangplank",
+            "traits": ["Scrap", "Form Swapper", "Pit Fighter"],
+            "cost": 3
+        },
+        {
+            "name": "Kog'Maw",
+            "traits": ["Automata", "Sniper"],
+            "cost": 3
+        },
+        {
+            "name": "Loris",
+            "traits": ["Enforcer", "Sentinel"],
+            "cost": 3
+        },
+        {
+            "name": "Nami",
+            "traits": ["Emissary", "Sorcerer"],
+            "cost": 3
+        },
+        {
+            "name": "Nunu & Willump",
+            "traits": ["Experiment", "Bruiser", "Visionary"],
+            "cost": 3
+        },
+        {
+            "name": "Renni",
+            "traits": ["Chem-Baron", "Bruiser"],
+            "cost": 3
+        },
+        {
+            "name": "Scar",
+            "traits": ["Firelight", "Watcher"],
+            "cost": 3
+        },
+        {
+            "name": "Smeech",
+            "traits": ["Chem-Baron", "Ambusher"],
+            "cost": 3
+        },
+        {
+            "name": "Swain",
+            "traits": ["Conqueror", "Form Swapper", "Sorcerer"],
+            "cost": 3
+        },
+        {
+            "name": "Twisted Fate",
+            "traits": ["Enforcer", "Quickstriker"],
+            "cost": 3
+        }
+    ],
+    "four": [
+        {
+            "name": "Ambessa",
+            "traits": ["Emissary", "Conqueror", "Quickstriker"],
+            "cost": 4
+        },
+        {
+            "name": "Corki",
+            "traits": ["Scrap", "Artillerist"],
+            "cost": 4
+        },
+        {
+            "name": "Dr. Mundo",
+            "traits": ["Experiment", "Dominator"],
+            "cost": 4
+        },
+        {
+            "name": "Ekko",
+            "traits": ["Firelight", "Scrap", "Ambusher"],
+            "cost": 4
+        },
+        {
+            "name": "Elise",
+            "traits": ["Black Rose", "Form Swapper", "Bruiser"],
+            "cost": 4
+        },
+        {
+            "name": "Garen",
+            "traits": ["Emissary", "Watcher"],
+            "cost": 4
+        },
+        {
+            "name": "Heimerdinger",
+            "traits": ["Academy", "Visionary"],
+            "cost": 4
+        },
+        {
+            "name": "Illaoi",
+            "traits": ["Rebel", "Sentinel"],
+            "cost": 4
+        },
+        {
+            "name": "Silco",
+            "traits": ["Chem-Baron", "Dominator"],
+            "cost": 4
+        },
+        {
+            "name": "Twitch",
+            "traits": ["Experiment", "Sniper"],
+            "cost": 4
+        },
+        {
+            "name": "Vi",
+            "traits": ["Enforcer", "Pit Fighter"],
+            "cost": 4
+        },
+        {
+            "name": "Zoe",
+            "traits": ["Rebel", "Sorcerer"],
+            "cost": 4
+        }
+    ],
+    "five": [
+        {
+            "name": "Caitlyn",
+            "traits": ["Enforcer", "Sniper"],
+            "cost": 5
+        },
+        {
+            "name": "Jayce",
+            "traits": ["Academy", "Form Swapper"],
+            "cost": 5
+        },
+        {
+            "name": "Jinx",
+            "traits": ["Rebel", "Ambusher"],
+            "cost": 5
+        },
+        {
+            "name": "LeBlanc",
+            "traits": ["Black Rose", "Sorcerer"],
+            "cost": 5
+        },
+        {
+            "name": "Malzahar",
+            "traits": ["Automata", "Visionary"],
+            "cost": 5
+        },
+        {
+            "name": "Mordekaiser",
+            "traits": ["Conqueror", "Dominator"],
+            "cost": 5
+        },
+        {
+            "name": "Rumble",
+            "traits": ["Junker King", "Scrap", "Sentinel"],
+            "cost": 5
+        },
+        {
+            "name": "Sevika",
+            "traits": ["High Roller", "Chem-Baron", "Pit Fighter"],
+            "cost": 5
+        },
+    ],
+    "six": [
+        {
+            "name": "Mel",
+            "traits": ["Banished Mage"],
+            "cost": 6
+        },
+        {
+            "name": "Viktor",
+            "traits": ["Machine Herald"],
+            "cost": 6
+        },
+        {
+            "name": "Warwick",
+            "traits": ["Experiment", "Blood Hunter"],
+            "cost": 6
+        }
+    ]
+}
+export default champs
